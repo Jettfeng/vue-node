0ec208399a0c474be83a505a664734a23262c03f
jettFeng <1130034920@>
2018-04-03 22:06:27 +0800

    commit

---
+++ src/views/GoodList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      这是商品列表页面
+      <span>{{$route.params.goodsId}}</span>
+      <span>{{$route.params.name}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "good-list"
+    }
+</script>
+
+<style scoped>
+
+</style>
